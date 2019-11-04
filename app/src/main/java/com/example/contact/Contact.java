f073a0a51f2a42cb2272b5f1030d4b3d2d569ffd
dodywahyupratama <dodywahyupra@gmail.com>
2019-11-04 22:26:57 +0700

    SQLite Contact

---
+++ app/src/main/java/com/example/contact/Contact.java
@@ -0,0 +1,49 @@
+package com.example.contact;
+
+/**
+ * Created by USER on 01/11/2019
+ */
+
+public class Contact {
+    String name, email, uname, pass;
+
+    public void setName (String name)
+    {
+        this.name=name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setEmail (String email)
+    {
+        this.email=email;
+    }
+
+    public String getEmail()
+    {
+        return this.name;
+    }
+
+    public void setUname (String uname)
+    {
+        this.uname=uname;
+    }
+
+    public String getUname()
+    {
+        return this.uname;
+    }
+
+    public void setPass (String pass)
+    {
+        this.pass=pass;
+    }
+
+    public String getPass()
+    {
+        return this.pass;
+    }
+}
